5796e8b9e8936d1b3290a400245a1a3d0ac6ffd7
moisesvillalba <moisesvillalba@gmail.com>
2023-10-02 17:55:43 -0300

    20231002 utilizar casfpuna

--- djoserauthapi/settings.py
+++ djoserauthapi/settings.py
@@ -81,23 +81,37 @@ WSGI_APPLICATION = 'djoserauthapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#      }
+# }
+
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.postgresql',
+#          'NAME': 'cas',  # Reemplaza con el nombre de tu base de datos
+#          'USER': 'cas',    # Reemplaza con tu usuario de PostgreSQL
+#          'PASSWORD': '--cas--',  # Reemplaza con tu contraseña de PostgreSQL
+#          'HOST': 'desarrollodtic.pol.una.py',  # Reemplaza con la dirección del servidor de la base de datos si es necesario
+#          'PORT': '5432',       # Puerto predeterminado de PostgreSQL
+#      }
+# }
+
+
 DATABASES = {
      'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'cas',  # Reemplaza con el nombre de tu base de datos
+         'USER': 'miusuario',    # Reemplaza con tu usuario de PostgreSQL
+         'PASSWORD': 'mipass',  # Reemplaza con tu contraseña de PostgreSQL
+         'HOST': 'desarrollodtic.pol.una.py',  # Reemplaza con la dirección del servidor de la base de datos si es necesario
+         'PORT': '5432',       # Puerto predeterminado de PostgreSQL
      }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'cas',  # Reemplaza con el nombre de tu base de datos
-#         'USER': 'cas',    # Reemplaza con tu usuario de PostgreSQL
-#         'PASSWORD': '--cas--',  # Reemplaza con tu contraseña de PostgreSQL
-#         'HOST': 'localhost',  # Reemplaza con la dirección del servidor de la base de datos si es necesario
-#         'PORT': '5432',       # Puerto predeterminado de PostgreSQL
-#     }
-# }
+
 
 
 # Email Configuration
